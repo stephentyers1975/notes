03044d1e4fbd8dd173dc7517c9b5f3aa6ec854fc
stephentyers1975 <steve.tyers@gmail.com>
2025-06-02 11:23:40 +0100

    updated backend

--- terraform/azure/project_11_devops/dev/backend.tf
+++ terraform/azure/project_11_devops/dev/backend.tf
@@ -1,9 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfdevbackend2024piyush"
-    container_name      = "tfstate"
-    key                 = "dev.tfstate"
+    
   }
 }
 
--- terraform/azure/project_11_devops/staging/backend.tf
+++ terraform/azure/project_11_devops/staging/backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstagebackend2024piyush"
-    container_name      = "tfstate"
-    key                 = "stage.tfstate"
   }
 }
 
